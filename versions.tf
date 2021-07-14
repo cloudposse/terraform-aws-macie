ea4267ba843220d8dbdfbe2889b38b233539fe23
Benjamin Smith <ben.smith.developer@gmail.com>
2021-07-14 10:41:36 -0700

    remove excess providers

--- versions.tf
+++ versions.tf
@@ -3,14 +3,6 @@ terraform {
 
   required_providers {
     # Update these to reflect the actual requirements of your module
-    local = {
-      source  = "hashicorp/local"
-      version = ">= 1.2"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = ">= 2.2"
-    }
     aws = {
       source  = "hashicorp/aws"
       version = ">= 2.0"
